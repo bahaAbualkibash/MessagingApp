c76936ee7bdcd9164dbb547987b4e6308708b350
Baha <baha.ra3d@gmail.com>
2022-02-21 13:54:56 +0200

    First

---
+++ MessagingApp/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using MessagingApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MessagingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> getUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> getUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+           
+        }
+    }
+}
